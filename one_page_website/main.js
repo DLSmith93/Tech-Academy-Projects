68f6a225bd8e5fcee17bf773436ec2adfd2a47a2
DLSmith93 <baumbor@hotmail.com>
2023-11-23 12:54:32 -0800

    added new features to one page website

--- one_page_website/main.js
+++ one_page_website/main.js
@@ -41,3 +41,12 @@ function showSlides(n){
     dots[slideIndex - 1].className = " active";
     captionText.innerHTML = dots[slideIndex = 1].alt;
 }
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+  
